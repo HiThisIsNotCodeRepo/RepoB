3e09da39b3df28d44c32d39a0f0ae4d81e991496
qinchenfeng1 <qinchenfeng1@spgroup.com.sg>
2025-02-15 08:35:35 +0800

    First commit

---
+++ bar.cpp
@@ -0,0 +1,9 @@
+//
+// Created by PQIC0964 on 15/2/25.
+//
+
+#include "bar.h"
+
+int bar::add(int a, int b) {
+    return a + b;
+}
---
+++ bar.h
@@ -0,0 +1,14 @@
+//
+// Created by PQIC0964 on 15/2/25.
+//
+
+#ifndef REPOB_BAR_H
+#define REPOB_BAR_H
+
+
+class bar {
+    int add(int a, int b);
+};
+
+
+#endif //REPOB_BAR_H
